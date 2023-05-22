cd2fa80cef51e19661b396e024f638cc8c423627
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-05-23 01:10:47 +0630

    setup with http server using gin

---
+++ api/server.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	db "github.com/HtetOoNaing/simple-bank-backend-master-class-golang-postgres-kubernetes-gRPC/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves HTTP requests for our banking services
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// add routes to router
+	router.POST("/accounts", server.createAccount)
+
+	server.router = router
+	return server
+}
