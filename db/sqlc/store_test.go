d3c6b76dd84173cf25205e56eb7c9bb816c4541d
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-05-22 12:44:04 +0630

    add transfer test to TextTransferTx

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -39,5 +39,16 @@ func TestTransferTx(t *testing.T) {
 		result := <-results
 		require.NotEmpty(t, result)
 
+		// check Transfer
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, account1.ID, transfer.FromAccountID)
+		require.Equal(t, account2.ID, transfer.ToAccountID)
+		require.Equal(t, amount, transfer.Amount)
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
 	}
 }
