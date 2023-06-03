7795a68282d08df088fcdab8606c2fa5d7f446f1
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-06-03 16:03:19 +0630

    add createUser and user route

--- api/server.go
+++ api/server.go
@@ -30,6 +30,9 @@ func NewServer(store db.Store) *Server {
 	// transfers
 	router.POST("/transfers", server.createTransfer)
 
+	// users
+	router.POST("/users", server.createUser)
+
 	server.router = router
 	return server
 }
